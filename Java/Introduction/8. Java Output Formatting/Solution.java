77ac167d477bf49934947393f3e7b78ee3d36c10
mohitrajvardhan17 <mohitrajvardhan17@gmail.com>
2018-12-23 13:31:09 +0530

    updating Comments '8. Java Output Formatting' for HackerRank Java solution

--- Java/Introduction/8. Java Output Formatting/Solution.java
+++ Java/Introduction/8. Java Output Formatting/Solution.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        /* Read input */
         Scanner scan = new Scanner(System.in);
         System.out.println("================================");
         for (int i = 0; i < 3; i++) {
             String s1 = scan.next();
             int x = scan.nextInt();
+            /* Print output */
             System.out.format("%-15s%03d%n", s1, x);
         }
         System.out.println("================================");
