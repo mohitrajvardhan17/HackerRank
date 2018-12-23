0354d50243a5ba66f974f87735d16151126ec80b
mohitrajvardhan17 <mohitrajvardhan17@gmail.com>
2018-12-23 13:33:59 +0530

    Adding '9. Java End-of-file' for HackerRank Java Solution

---
+++ Java/Introduction/9. Java End-of-file/Solution.java
@@ -0,0 +1,20 @@
+//     Author: Mohit Rajvardhan
+//     Github: github.com/mohitrajvardhan17
+// HackerRank: hackerrank.com/mohirajvardhan17
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] argh) {
+        /* Read input */
+        Scanner scan = new Scanner(System.in);
+        int i = 1;
+
+        /* Print output */
+        while (scan.hasNextLine()) {
+            System.out.println(i + " " + scan.nextLine());
+            i++;
+        }
+        scan.close();
+    }
+}
